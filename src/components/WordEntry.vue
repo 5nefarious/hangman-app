d283a2f3d452e718145334ab752b63a6f64a16e6
Arvin Ignaci <arvin.ignaci@gmail.com>
2022-01-21 23:41:13 -0700

    Create main UI components

---
+++ src/components/WordEntry.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="word-entry">
+    <form @submit.prevent="$emit('select', word)">
+      <input type="text" v-model="word" pattern="[a-z]+" required>
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordEntry',
+  emits: ['select'],
+  data() {
+    return { word: '' }
+  }
+}
+</script>
+
+<style scoped>
+.word-entry {
+  margin: 1em;
+}
+</style>
