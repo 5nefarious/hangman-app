8e46f88f436b567ce28556db6b567c991c0f1339
Arvin Ignaci <arvin.ignaci@gmail.com>
2022-01-23 02:39:17 -0700

    Complete base game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="game" class="container" :style="gridStyle">
-    <div class="guessed-letter grid-item" v-for="char, i in guesses"
-     :key="i" :style="{ 'grid-column': i + 2 }">
+    <div class="known-letter grid-item" v-for="char, i in known"
+     :key="i" :style="gridCellStyle({ col: i + 2 })">
      <h1>{{ char }}</h1>
     </div>
     <div class="row-label grid-item" v-for="char, i in chars"
-     :key="i" :style="{ 'grid-row': i + 2 }">
+     :key="i" :style="gridCellStyle({ row: i + 2 })">
       {{ char }}
     </div>
-    <div class="prob-cell grid-item" v-for="{ value, index }, i in probs"
-     :key="i" :style="{ 'grid-row': index[0] + 2, 'grid-column': index[1] + 2 }">
+    <div class="prob-cell grid-item" v-for="{ value, index }, i in probs" :key="i"
+     :style="gridCellStyle({ row: index[0] + 2, col: index[1] + 2, alpha: value })">
       <small>{{ displayProb(value) }}</small>
     </div>
-    <div class="prob-cell grid-item" v-for="{ value, index }, i in charProbs"
-     :key="i" :style="{ 'grid-row': index[0] + 2, 'grid-column': -1 }">
+    <div class="prob-cell grid-item" v-for="{ value, index }, i in charProbs" :key="i"
+     :style="gridCellStyle({ row: index[0] + 2, col: -1, alpha: value })">
       <small>{{ displayProb(value) }}</small>
     </div>
   </div>
@@ -36,12 +36,17 @@ export default {
       type: Number,
       default: 1.0
     },
+    endpoint: {
+      type: String,
+      default: "http://localhost:5000"
+    }
   },
   data() {
     return {
       chars: "",
-      guesses: "",
+      known: "",
       probs: math.matrix(),
+      guessed: "",
     }
   },
   computed: {
@@ -57,22 +62,85 @@ export default {
       } catch (err) {
         return math.zeros(this.chars.length, 1)
       }
+    },
+    isKnown() {
+      return this.known.split('').map((char) => char != '_')
     }
   },
   methods: {
-    resetBoard(word) {
-      this.guesses = '_'.repeat(word.length)
+    resetState(word) {
+      this.known = '_'.repeat(word.length)
       this.probs = math.zeros(this.chars.length, word.length)
+      this.guessed = ""
     },
     displayProb(val) {
       return (100 * val).toFixed(1)
-    }
+    },
+    async updateProbs() {
+      const url = new URL(this.endpoint)
+      url.searchParams.append('known', this.known)
+      url.searchParams.append('excluded', this.guessed)
+      const response = await fetch(url)
+      const { chars, counts } = await response.json()
+      if (!this.chars) this.chars = chars
+      this.probs = math.zeros(this.chars.length, this.word.length)
+      {
+        let add = (a, b) => a + b
+        let total = counts.map((row) => row.reduce(add, 0)).reduce(add, 0)
+        for (let m = 0; m < counts.length; m++) {
+          let char = chars[m]
+          let i = this.chars.indexOf(char), j = 0
+          for (let n = 0; n < counts[m].length; n++) {
+            while (this.isKnown[j]) { j++ }
+            this.probs.set([i, j], counts[m][n] / total)
+            j++
+          }
+        }
+      }
+      this.makeGuess()
+    },
+    makeGuess() {
+      var maxIdx = [], maxProb = 0
+      for (let i = 0; i < this.chars.length; i++) {
+        if (this.guessed.includes(this.chars[i])) continue
+        let val = this.charProbs.get([i, 0])
+        if (val == maxProb) {
+          maxIdx.push(i)
+        } else if (val > maxProb) {
+          maxIdx = [i]
+          maxProb = val
+        }
+      }
+      var idx = math.pickRandom(maxIdx)
+      var char = this.chars[idx]
+      var known = this.known.split('')
+      for (let i = 0; i < this.word.length; i++) {
+        if (this.word[i] == char) known[i] = char
+      }
+      this.known = known.join('')
+      this.guessed += char
+      console.debug('guessed: ' + this.guessed)
+      if (this.known.indexOf('_') > -1) {
+        window.setTimeout(this.updateProbs, 1000 * this.delay)
+      }
+    },
+    gridCellStyle({row, col, alpha}) {
+      var style = { }
+      if (row) style['grid-row'] = row
+      if (col) style['grid-column'] = col
+      if (alpha) {
+        style['opacity'] = (alpha / 2) + 0.5
+        style['background-color'] = `hsla(140, 50%, 30%, ${alpha})`
+      }
+      return style
+    },
   },
   watch: {
-    word(newWord) { this.resetBoard(newWord) }
+    word(newWord) { this.resetState(newWord) }
   },
   mounted() {
-    this.resetBoard(this.word)
+    this.resetState(this.word)
+    this.updateProbs()
   }
 }
 </script>
@@ -92,14 +160,16 @@ export default {
   grid-column: 1;
 }
 
-.guessed-letter {
+.known-letter {
   grid-row: 1;
 }
 
 .prob-cell {
   width: 3em;
   height: 2em;
-  color: var(--dim-color);
+  opacity: 20%;
+  border-radius: 0.2em;
+  margin: 0.1em;
 }
 
 #game {
